08ef8c78753efa93805e8efcf7376666a8f8e8b9
hyunweezy <choi.jhw@gmail.com>
2024-02-26 17:16:36 -0500

    Update scripts.js
    changed array name to "pokemonDirectory"

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonList = [];
 
-let pokemonList = [
+let pokemonDirectory = [
     {pokemon: 'Bulbasaur', height: '0.7', types: ['grass', 'poison']},
     {pokemon: 'Charmander', height: '0.6', types: 'fire'},
     {pokemon: 'Squirtle', height: '0.5', types: 'water'},
