0e4c53077955c14d155e59d44b611a7e8b3fbc42
hyunweezy <choi.jhw@gmail.com>
2024-02-26 15:32:38 -0500

    Update scripts.js
    added blank array for pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+let pokemonList = [];
+
+
+
+
 alert('Hello world');
 let favoriteFood = 'pizza';
 document.write(favoriteFood);
+
+let name = 'John';
+let welcomeMessage1 = "Hello, I'm";
+let welcomeMessage2 = 'and I say to you: "Hello!"';
+console.log(welcomeMessage1 + ' ' + name + ' ' + welcomeMessage2);
