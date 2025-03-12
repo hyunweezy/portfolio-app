cbce208463057bed39eefa76268986d30c30ec23
hyunweezy <choi.jhw@gmail.com>
2025-03-12 13:49:36 -0400

    Add IIFE
    Change for loop to forEach and wrap script in IIFE.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,49 @@
-let pokemonList = [
-        {name: 'Bulbasaur', height: '0.7', types: ['grass', 'poison']},
-        {name: 'Ivysaur', height: '1.0', types: ['grass', 'poison']},
-        {name: 'Venusaur', height: '2.0', types: ['grass', 'poison']},
-        {name: 'Charmander', height: '0.6', types: ['fire']},
-        {name: 'Charmeleon', height: '1.1', types: ['fire']},
-        {name: 'Charizard', height: '1.7', types: ['fire', 'flying']},
-        {name: 'Squirtle', height: '0.5', types: ['water']},
-        {name: 'Wartortle', height: '1.0', types: ['water']},
-        {name: 'Blastoise', height: '1.6', types: ['water']},
-    ]
+let pokemonRepository = (function() {
+    let pokemonList = [
+            {name: 'Bulbasaur', height: '0.7', types: ['grass', 'poison']},
+            {name: 'Ivysaur', height: '1.0', types: ['grass', 'poison']},
+            {name: 'Venusaur', height: '2.0', types: ['grass', 'poison']},
+            {name: 'Charmander', height: '0.6', types: ['fire']},
+            {name: 'Charmeleon', height: '1.1', types: ['fire']},
+            {name: 'Charizard', height: '1.7', types: ['fire', 'flying']},
+            {name: 'Squirtle', height: '0.5', types: ['water']},
+            {name: 'Wartortle', height: '1.0', types: ['water']},
+            {name: 'Blastoise', height: '1.6', types: ['water']},
+        ]
 
-pokemonList.forEach(function(pokemon) {
-    console.log(pokemon);
+    function add(pokemon) {
+        if (typeof pokemon === 'object' && pokemon !== null) {
+            pokemonList.push(pokemon);
+        }
+        else {
+            console.log('Only objects can be added');
+        }
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return{
+        add: add,
+        getAll: getAll
+    };
+
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+    console.log(pokemon.name + " height: " + pokemon.height + " types: " + pokemon.types)
 });
 
+// pokemonRepository.Object.keys(pokemonList).getAll().forEach(function(pokemon) {
+//         console.log();
+//     });
 
+// adds Pikachu to pokemonList
 
-
-
-
+// console.log(pokemonRepository.getAll());
+// pokemonRepository.add({name: 'Pikachu'});
+// console.log(pokemonRepository.getAll());
 
 
 
@@ -44,13 +69,13 @@ pokemonList.forEach(function(pokemon) {
 
 // improves composition of for loop (need to revise to accomodate objects vs strings)
 
-// function getHeightDescription(height){
+// function getHeightDescription(height) {
 //     return height;
 // }
-// function getTypeDescription(types){
+// function getTypeDescription(types) {
 //     return types;
 // }
-// function getPokemonDescription(pokemon){
+// function getPokemonDescription(pokemon) {
 //     let heightDescription = getHeightDescription(pokemon.height);
 //     let typeDescription = getTypeDescription(pokemon.types);
 //     return pokemon.name + ', ' + heightDescription + ', ' + typeDescription;
@@ -59,20 +84,20 @@ pokemonList.forEach(function(pokemon) {
 // this is Object.keys() function that creates an array of all the keys in the object 
 // pokemonList. this only executes when array is in bracket notation.
 
-// Object.keys(pokemonList2).forEach(function(property){
+// Object.keys(pokemonList2).forEach(function(property) {
 //     document.write(property + ': ' + pokemonList2[property] + '<br>');
 // });
 
 // this is an internal anonymous function
 
-// pokemonList.forEach(function(name, index, array){
+// pokemonList.forEach(function(name, index, array) {
 //     console.log(name, index + array);
 // })
 
 // this forEach() function to loop over an array of objects
 
-// pokemonList.forEach(function(pokemon){
-//     if (pokemon.height > 1.1){
+// pokemonList.forEach(function(pokemon) {
+//     if (pokemon.height > 1.1)
 //         sizeDescription = ' - Wow, that\'s big!';
 //         document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')' + sizeDescription + '</xp>')
 //     } else {
@@ -82,7 +107,7 @@ pokemonList.forEach(function(pokemon) {
 
 // this utilizes function declaration with parameters and is then called
 
-// function printArrayDetails(list){
+// function printArrayDetails(list) {
 //     for (let i = 0; i < list.length; i++) {
 //         if(list[i].height > 1.1){
 //             sizeDescription = ' - Wow, that\'s big!';
